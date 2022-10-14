76da2784a33ea9f5ef55fee1707b72d80ec573b8
asliddin <asliddintojaliyev88@gmail.com>
2022-10-15 02:22:14 +0500

    commit

--- publisher/src/main/java/com/example/publisher/PublisherApplication.java
+++ publisher/src/main/java/com/example/publisher/PublisherApplication.java
@@ -2,7 +2,6 @@ package com.example.publisher;
 
 import lombok.*;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Formula;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,8 +15,9 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -72,6 +72,30 @@ interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 @Service
 @RequiredArgsConstructor
+class TransactionService {
+    private final TransactionRepository repository;
+    private final KafkaService kafkaService;
+
+    public Transaction create(TransferCreateVO vo) {
+        Transaction transaction = Transaction.builder()
+                .pan(vo.getPan())
+                .amount(vo.getAmount())
+                .build();
+        transaction = repository.save(transaction);
+        kafkaService.send(transaction);
+        return transaction;
+    }
+}
+
+@RestController
+@RequestMapping("/transfer")
+@RequiredArgsConstructor
+class PublisherController {
+    private final TransactionService service;
+}
+
+@Service
+@RequiredArgsConstructor
 class KafkaService {
     public final String TOPIC = "pdp-topic";
     private final KafkaTemplate<String, Object> kafkaTemplate;
@@ -87,16 +111,16 @@ class KafkaProducerConfig {
     private String bootstrapServers;
 
     @Bean
-    public ProducerFactory<String,Object> producerFactory(){
-        Map<String,Object> configProps=new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String,Object> kafkaTemplate(){
+    public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
