85dc0d10b62e200040e52ecbaed8f8d91778c0d7
asliddin <asliddintojaliyev88@gmail.com>
2022-10-15 01:46:44 +0500

    commit

--- publisher/src/main/java/com/example/publisher/PublisherApplication.java
+++ publisher/src/main/java/com/example/publisher/PublisherApplication.java
@@ -1,13 +1,45 @@
 package com.example.publisher;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Formula;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.kafka.annotation.EnableKafka;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
 
 @SpringBootApplication
+@EnableKafka
 public class PublisherApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PublisherApplication.class, args);
     }
+
+}
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+class Transaction{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String pan;
+    @Column(nullable = false)
+    @Formula("amount > 0.0")
+    private BigDecimal amount;
+    @CreatedDate
+    @CreationTimestamp
+    @Column(columnDefinition = "timestamp default current_timestamp")
+    private Timestamp createdAt;
 
 }
