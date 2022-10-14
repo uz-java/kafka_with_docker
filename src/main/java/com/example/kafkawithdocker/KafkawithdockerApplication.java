5c8a21d1d52e7f7ca7188717055763cf14986787
asliddin <asliddintojaliyev88@gmail.com>
2022-10-15 01:30:00 +0500

    commit

---
+++ src/main/java/com/example/kafkawithdocker/KafkawithdockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkawithdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkawithdockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkawithdockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkawithdocker/KafkawithdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkawithdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkawithdockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
