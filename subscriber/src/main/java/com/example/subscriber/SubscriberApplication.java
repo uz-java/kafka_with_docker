2dbfcc1e52e54e2c16510830410d46228730f0c5
asliddin <asliddintojaliyev88@gmail.com>
2022-10-15 22:52:17 +0500

    commit

--- subscriber/src/main/java/com/example/subscriber/SubscriberApplication.java
+++ subscriber/src/main/java/com/example/subscriber/SubscriberApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SubscriberApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SubscriberApplication.class, args);
     }
