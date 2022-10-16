2f4b914347f8b6e40c37216dd1932143acd98b04
asliddin <asliddintojaliyev88@gmail.com>
2022-10-16 11:31:25 +0500

    commit

--- subscriber/src/main/java/com/example/subscriber/SubscriberApplication.java
+++ subscriber/src/main/java/com/example/subscriber/SubscriberApplication.java
@@ -1,14 +1,24 @@
 package com.example.subscriber;
 
 import lombok.*;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableKafka
@@ -41,4 +51,23 @@ class TransactionListener {
 class Constants {
     public static final String TOPIC="pdp-topic";
     public static final String GROUP_ID="group-id";
+}
+
+@Configuration
+class KafkaConsumerConfig{
+    @Value("${kafka.server}")
+    private String bootstrapServers;
+
+    @Bean
+    public ConsumerFactory<String,Transaction> consumerFactory(){
+        Map<String,Object> config=new HashMap<>();
+        JsonDeserializer<Transaction> deserializer=new JsonDeserializer<>(Transaction.class);
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_id");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,deserializer);
+        return new DefaultKafkaConsumerFactory<>(config,new StringDeserializer(),deserializer);
+    }
+
+
 }
